9e6c228476b1735b9b9d23e51c9beddef007b712
Hari Veerendra <veerendrahari396@gmail.com>
2023-08-25 22:06:57 +0530

    sign up page is created and also firebase connected to login page

--- account.js
+++ account.js
@@ -1,29 +1,22 @@
-import fs from "fs";
-function file(lit) {
-  fs.open("accountdetailes.txt", "a+", (err, file) => {
-    if (err) throw err;
-    fs.writeFile(file, lit, (err) => {
-      if (err) throw err;
-      console.log("Written on File!");
-    });
-  });
-}
-function submitform() {
-  event.preventDefault();
-}
 function account_create() {
-  const acct =
-    Math.round(Math.random() * (100000000000 - 10000000000 + 1)) + 10000000000;
   document.getElementById("sub").disabled = true;
-  console.log(acct);
-
-  document.getElementById(
-    "account_num"
-  ).innerHTML = `Your Account Number : ${acct} `;
-  const lit = [];
-  lit[0] = acct;
-  for (var i = 1; i <= 16; i++) {
-    lit[i] = document.getElementById(i).value;
+  localStorage.clear();
+  window.open("accountNum.html", "blank");
+  window.close("Account.html");
+}
+function acc() {
+  val_ue = localStorage.getItem("accnum");
+  if (!val_ue) {
+    const acct =
+      Math.round(Math.random() * (100000000000 - 10000000000 + 1)) +
+      10000000000;
+    document.getElementById(
+      "account_num"
+    ).innerHTML = `Your Account Number : ${acct}`;
+    localStorage.setItem("accnum", acct);
+  } else {
+    document.getElementById(
+      "account_num"
+    ).innerHTML = `Your Account Number : ${val_ue}`;
   }
-  console.log(lit);
 }
