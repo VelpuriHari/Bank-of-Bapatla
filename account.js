a4b1b6c49e9773cef31b8a105770975ec39b8ae2
Hari Veerendra <veerendrahari396@gmail.com>
2023-08-27 10:24:26 +0530

    Creating Account is completed and heading towards transactions

--- account.js
+++ account.js
@@ -1,22 +1,13 @@
-function account_create() {
-  document.getElementById("sub").disabled = true;
-  localStorage.clear();
-  window.open("accountNum.html", "blank");
-  window.close("Account.html");
-}
 function acc() {
-  val_ue = localStorage.getItem("accnum");
-  if (!val_ue) {
-    const acct =
-      Math.round(Math.random() * (100000000000 - 10000000000 + 1)) +
-      10000000000;
-    document.getElementById(
-      "account_num"
-    ).innerHTML = `Your Account Number : ${acct}`;
-    localStorage.setItem("accnum", acct);
-  } else {
-    document.getElementById(
-      "account_num"
-    ).innerHTML = `Your Account Number : ${val_ue}`;
+  localStorage.setItem("email", "veerendrahari396@gmail.com");
+  var mail = localStorage.getItem("email");
+  document.getElementById("mail").innerHTML = `${mail}`;
+}
+function verify() {
+  var code = document.getElementById("code").value;
+  if (code == localStorage.getItem("pin")) {
+    document.getElementById("acc_num").innerHTML = `${localStorage.getItem(
+      "accnum"
+    )}`;
   }
 }
