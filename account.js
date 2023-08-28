4088929b500a96c361b4dc5ccfb3de3d06e0f9de
Hari Veerendra <veerendrahari396@gmail.com>
2023-08-28 20:13:25 +0530

    Depotion page is completed and balance element is added

--- account.js
+++ account.js
@@ -3,7 +3,7 @@ function acc() {
   var mail = localStorage.getItem("email");
   document.getElementById("mail").innerHTML = `${mail}`;
 }
-function verify() {
+function acc_num() {
   var code = document.getElementById("code").value;
   if (code == localStorage.getItem("pin")) {
     document.getElementById("acc_num").innerHTML = `${localStorage.getItem(
